5551695ba38f56fc4341855d693789f9b11e307e
Dmytro <dimamel28@gmail.com>
2024-04-30 23:55:46 +0300

    From MYSQL to POSTGRESQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id BIGSERIAL PRIMARY KEY,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     password VARCHAR(125) NOT NULL,
@@ -9,19 +9,26 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS bank_accounts (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    balance DOUBLE NOT NULL DEFAULT 0,
-    user_id BIGINT NOT NULL UNIQUE,
+    id BIGSERIAL PRIMARY KEY,
+    balance DOUBLE PRECISION NOT NULL DEFAULT 0
+);
 
-    FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE IF NOT EXISTS user_bank_account (
+    user_id BIGINT NOT NULL,
+    bank_account_id BIGINT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (bank_account_id) REFERENCES bank_accounts(id),
+
+    PRIMARY KEY (user_id, bank_account_id)
 );
 
 CREATE TABLE IF NOT EXISTS transactions (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id BIGSERIAL PRIMARY KEY,
     bank_account_id BIGINT NOT NULL,
     msg VARCHAR(50) NOT NULL,
     type VARCHAR(50) NOT NULL,
-    money_amount DOUBLE NOT NULL,
+    money_amount DOUBLE PRECISION NOT NULL,
     transaction_date timestamp NOT NULL,
 
     FOREIGN KEY (bank_account_id) REFERENCES bank_accounts(id)
