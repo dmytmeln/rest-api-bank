890849cb716d05919538734c1e13c100c02ff692
Dmytro <dimamel28@gmail.com>
2024-04-30 22:53:49 +0300

    Transaction transfer

--- src/main/java/bank/model/Transaction.java
+++ src/main/java/bank/model/Transaction.java
@@ -1,41 +1,33 @@
 package bank.model;
 
-import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Data
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 @Table(name = "transactions")
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     private BankAccount bankAccount;
 
-    @Column
     private String msg;
 
-    @Column
     private String type;
 
-    @Column
     @Builder.Default
     private Double moneyAmount = 0D;
 
-    @Column
-    @CreationTimestamp
-    @EqualsAndHashCode.Exclude
-    private Instant transactionDate;
+    @Builder.Default
+    private LocalDateTime transactionDate = LocalDateTime.now();
 
 }
