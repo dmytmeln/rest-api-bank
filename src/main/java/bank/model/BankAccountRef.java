dd4836d250cded7b90193d7e9adda350e670bad8
Dmytro <dimamel28@gmail.com>
2024-04-30 22:53:17 +0300

    Implemented many-to-many relationship between User and BankAccount

---
+++ src/main/java/bank/model/BankAccountRef.java
@@ -0,0 +1,16 @@
+package bank.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("user_bank_account")
+public class BankAccountRef {
+
+    private Long bankAccountId;
+
+}
