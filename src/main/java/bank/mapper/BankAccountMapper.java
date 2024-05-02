800966439054a20a80406b162bd3965372e94a2b
Dmytro Melnyk <dimamel28@gmail.com>
2024-05-02 17:41:42 +0300

    Added mapper interface

--- src/main/java/bank/mapper/BankAccountMapper.java
+++ src/main/java/bank/mapper/BankAccountMapper.java
@@ -2,35 +2,9 @@ package bank.mapper;
 
 import bank.dto.bank.BankResponseDto;
 import bank.model.BankAccount;
-import bank.model.Transaction;
-import org.modelmapper.Converter;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeMap;
-import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
+public interface BankAccountMapper {
 
-@Component
-public class BankAccountMapper {
-
-    private final TypeMap<BankAccount, BankResponseDto> typeMap;
-    private final ModelMapper modelMapper;
-
-    public BankAccountMapper(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-
-        Converter<List<Transaction>, List<Long>> listConverter = converter -> converter.getSource().stream()
-            .map(Transaction::getId)
-            .collect(Collectors.toList());
-        this.typeMap = modelMapper.createTypeMap(BankAccount.class, BankResponseDto.class)
-            .addMappings(
-                mapper -> mapper.using(listConverter).map(BankAccount::getTransactions, BankResponseDto::setTransactionsId)
-            );
-    }
-
-    public BankResponseDto mapToBankResponseDto(BankAccount bankAccount) {
-        return typeMap.map(bankAccount);
-    }
+    BankResponseDto mapToBankResponseDto(BankAccount bankAccount);
 
 }
