2e400b4bc67253149f7544120f565e36c2434832
Dmytro Melnyk <dimamel28@gmail.com>
2024-05-11 19:33:28 +0300

    Added Builder annotation

--- src/main/java/bank/dto/transaction/TransactionResponseDto.java
+++ src/main/java/bank/dto/transaction/TransactionResponseDto.java
@@ -1,12 +1,14 @@
 package bank.dto.transaction;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class TransactionResponseDto {
     private Long id;
     private String msg;
