743499ad99e9425da914564ba34a07344ea6ebc2
Dmytro Melnyk <dimamel28@gmail.com>
2024-05-12 01:25:11 +0300

    Created BankIntegrationTest

--- src/test/java/bank/BankIntegrationTest.java
+++ src/test/java/bank/BankIntegrationTest.java
@@ -1,93 +1,200 @@
-//package bank;
-//
-//import bank.model.Transaction;
-//import bank.service.TransactionService;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-//@SpringBootTest
-//@ActiveProfiles("test")
-//@AutoConfigureMockMvc
-//public class BankIntegrationTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private TransactionService transactionService;
-//
-//    private final Long ACTUAL_ACCOUNT_ID = 1L;
-//
-//    private static Transaction expectedTransaction;
-//
-//    @BeforeAll
-//    public static void init() {
-//        expectedTransaction = Transaction.builder()
-//                .id(3L)
-//                .msg("Message")
-//                .type("Type")
-//                .moneyAmount(1000D)
-//                .build();
-//    }
-//
-//    @Test
-//    @Transactional
-//    public void makeDepositWithdrawalTest() throws Exception {
-//        makeDeposit();
-//        makeWithdrawal();
-//    }
-//
-//    @Transactional
-//    public void makeDeposit() throws Exception {
-//
-//        int expectedSize = 3;
-//        mockMvc.perform(post("/bank/deposit/{accountId}", ACTUAL_ACCOUNT_ID)
-//                        .param("moneyAmount",  expectedTransaction.getMoneyAmount().toString())
-//                        .param("msg", expectedTransaction.getMsg())
-//                        .param("type", expectedTransaction.getType()))
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(redirectedUrl("/bank"))
-//                .andExpect(view().name("redirect:/bank"));
-//
-//        List<Transaction> bankAccountTransactions = transactionService.getBankAccountTransactions(ACTUAL_ACCOUNT_ID);
-//
-//        assertEquals(expectedSize, bankAccountTransactions.size());
-//        Transaction actualTransaction = bankAccountTransactions.get(expectedSize - 1);
-//        assertEquals(expectedTransaction, actualTransaction);
-//
-//    }
-//
-//    @Transactional
-//    public void makeWithdrawal() throws Exception {
-//
-//        int expectedSize = 4;
-//        mockMvc.perform(post("/bank/withdrawal/{accountId}", ACTUAL_ACCOUNT_ID)
-//                        .param("moneyAmount",  expectedTransaction.getMoneyAmount().toString())
-//                        .param("msg", expectedTransaction.getMsg())
-//                        .param("type", expectedTransaction.getType()))
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(redirectedUrl("/bank"))
-//                .andExpect(view().name("redirect:/bank"));
-//
-//        List<Transaction> bankAccountTransactions = transactionService.getBankAccountTransactions(ACTUAL_ACCOUNT_ID);
-//
-//        assertEquals(expectedSize, bankAccountTransactions.size());
-//        Transaction actualTransaction = bankAccountTransactions.get(expectedSize - 1);
-//        expectedTransaction.setId(4L);
-//        assertEquals(expectedTransaction, actualTransaction);
-//
-//    }
-//
-//}
+package bank;
+
+import bank.dto.bank.BankResponseDto;
+import bank.dto.transaction.TransactionRequestDto;
+import bank.exception.EntityNotFoundException;
+import bank.model.BankAccount;
+import bank.service.BankService;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class BankIntegrationTest {
+
+    private final Long realUserId = 1L;
+    private final Long  realBankAccountId = 1L;
+
+    private final BankService bankService;
+
+    @Autowired
+    public BankIntegrationTest(BankService bankService) {
+        this.bankService = bankService;
+    }
+
+    @Test
+    public void testFindById() {
+        Double expectedBalance = 1000D;
+
+        BankAccount bankAccount = bankService.findById(realBankAccountId, realUserId);
+
+        assertNotNull(bankAccount);
+        assertEquals(realUserId, bankAccount.getId());
+        assertEquals(expectedBalance, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testFindById_nonExistingBankAccount() {
+        Long nonExistingBankAccountId = -1L,
+                nonExistingUserId = -1L;
+
+        assertThrows(
+                EntityNotFoundException.class,
+                () -> bankService.findById(nonExistingBankAccountId, nonExistingUserId)
+        );
+    }
+
+    @Test
+    public void testFindBankResponseById() {
+        Double expectedBalance = 1000D;
+
+        BankResponseDto bankResponseDto = bankService.findBankResponseById(realBankAccountId, realUserId);
+
+        assertNotNull(bankResponseDto);
+        assertEquals(realUserId, bankResponseDto.getId());
+        assertEquals(expectedBalance, bankResponseDto.getBalance());
+    }
+
+    @Test
+    public void testFindBankResponseById_nonExistingBankAccount() {
+        Long nonExistingBankAccountId = -1L,
+                nonExistingUserId = -1L;
+
+        assertThrows(
+                EntityNotFoundException.class,
+                () -> bankService.findBankResponseById(nonExistingBankAccountId, nonExistingUserId)
+        );
+    }
+
+    @Test
+    public void testDelete_existingAccount() {
+
+        boolean hasDeleted = bankService.deleteBankAccount(realBankAccountId, realUserId);
+
+        assertTrue(hasDeleted);
+        assertThrows(
+                EntityNotFoundException.class,
+                () -> bankService.findById(realBankAccountId, realUserId)
+        );
+
+    }
+
+    @Test
+    public void testDelete_nonExistingAccount() {
+        Long nonExistingBankAccountId = -1L,
+                nonExistingUserId = -1L;
+
+        assertThrows(
+                EntityNotFoundException.class,
+                () -> bankService.deleteBankAccount(nonExistingBankAccountId, nonExistingUserId)
+        );
+    }
+
+    @Test
+    public void testCreateBankAccountForUser() {
+        Double expectedBalance = 0D;
+        Long expectedBankResponseId = 2L;
+
+        BankResponseDto bankResponseDto = bankService.creteBankAccountForUser(realUserId);
+
+        assertNotNull(bankResponseDto);
+        assertEquals(expectedBankResponseId, bankResponseDto.getId());
+        assertEquals(expectedBalance, bankResponseDto.getBalance());
+    }
+
+    @Test
+    public void testUpdateUserBankAccount() {
+
+        double balance = 10000D;
+        BankAccount bankAccountToUpdate = BankAccount.builder()
+                .id(realBankAccountId)
+                .balance(balance)
+                .build();
+
+        BankAccount bankAccount = bankService.updateUserBankAccount(realUserId, bankAccountToUpdate);
+
+        assertEquals(realBankAccountId, bankAccount.getId());
+        assertEquals(balance, bankAccount.getBalance());
+
+    }
+
+    @Test
+    public void testUpdateUserBankAccount_nonExistingBankAccount() {
+        BankAccount bankAccountToUpdate = BankAccount.builder()
+                .id(-1L)
+                .balance(0D)
+                .build();
+
+        assertThrows(
+                EntityNotFoundException.class,
+                () -> bankService.updateUserBankAccount(realUserId, bankAccountToUpdate)
+        );
+    }
+
+
+    @Test
+    @Order(1)
+    public void testMakeDeposit() {
+
+        String type = "Transaction Type";
+        double moneyAmount = 1000D;
+        String msg = "Transaction Msg";
+        TransactionRequestDto transactionRequestDto = TransactionRequestDto.builder()
+                .type(type)
+                .moneyAmount(moneyAmount)
+                .msg(msg)
+                .build();
+
+        double bankAccountDbBalance = 1000D;
+        double expectedBalance = moneyAmount + bankAccountDbBalance;
+        int expectedSize = 3;
+        long expectedTransactionId = 3L;
+
+        BankResponseDto bankResponseDto = bankService.makeDeposit(realBankAccountId, realUserId, transactionRequestDto);
+
+        List<Long> transactionsId = bankResponseDto.getTransactionsId();
+        int size = transactionsId.size();
+        assertEquals(expectedSize, size);
+        assertEquals(expectedTransactionId, transactionsId.get(size - 1));
+        assertEquals(expectedBalance, bankResponseDto.getBalance());
+
+    }
+
+    @Test
+    @Order(2)
+    public void testMakeWithdrawal() {
+
+        String type = "Transaction Type";
+        double moneyAmount = 1000D;
+        String msg = "Transaction Msg";
+        TransactionRequestDto transactionRequestDto = TransactionRequestDto.builder()
+                .type(type)
+                .moneyAmount(moneyAmount)
+                .msg(msg)
+                .build();
+
+        double bankAccountDbBalance = 1000D;
+        double expectedBalance = bankAccountDbBalance - moneyAmount;
+        int expectedSize = 3;
+        long expectedTransactionId = 4L;
+
+        BankResponseDto bankResponseDto = bankService.makeWithdrawal(realBankAccountId, realUserId, transactionRequestDto);
+
+        List<Long> transactionsId = bankResponseDto.getTransactionsId();
+        int size = transactionsId.size();
+        assertEquals(expectedSize, size);
+        assertEquals(expectedTransactionId, transactionsId.get(size - 1));
+        assertEquals(expectedBalance, bankResponseDto.getBalance());
+
+    }
+
+}
