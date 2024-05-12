e0e82e44ec3c184761a02abe7cf7f8370587c424
Dmytro Melnyk <dimamel28@gmail.com>
2024-05-12 18:20:46 +0300

    Optimize imports

--- src/main/java/bank/configuration/Config.java
+++ src/main/java/bank/configuration/Config.java
@@ -3,10 +3,8 @@ package bank.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class Config {
