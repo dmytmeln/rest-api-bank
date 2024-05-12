28f11883f2526b1b714771241c7214bebf25ae53
Dmytro Melnyk <dimamel28@gmail.com>
2024-05-13 01:51:20 +0300

    Configured authorization

--- src/main/java/bank/controllers/TransactionController.java
+++ src/main/java/bank/controllers/TransactionController.java
@@ -1,8 +1,11 @@
 package bank.controllers;
 
+import bank.model.User;
 import bank.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,12 +16,14 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @GetMapping
-    public ResponseEntity<?> getBankAccountTransactions(@PathVariable Long bankAccountId) {
+    @PreAuthorize("hasRole('ADMIN') OR @userServiceImpl.hasBankAccount(#user, #bankAccountId)")
+    public ResponseEntity<?> getBankAccountTransactions(@AuthenticationPrincipal User user, @PathVariable Long bankAccountId) {
         return ResponseEntity.ok().body(transactionService.getBankAccountTransactions(bankAccountId));
     }
 
     @DeleteMapping
-    public ResponseEntity<?> clearBankAccountTransactions(@PathVariable Long bankAccountId) {
+    @PreAuthorize("hasRole('ADMIN') OR @userServiceImpl.hasBankAccount(#user, #bankAccountId)")
+    public ResponseEntity<?> clearBankAccountTransactions(@AuthenticationPrincipal User user, @PathVariable Long bankAccountId) {
 
         transactionService.clearBankAccountTransactions(bankAccountId);
         return ResponseEntity.noContent().build();
