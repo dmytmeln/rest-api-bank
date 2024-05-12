a1c90970372cc4c76b9ceb80d73c0754187e0be6
Dmytro Melnyk <dimamel28@gmail.com>
2024-05-12 18:36:34 +0300

    Created Jwt Filter

---
+++ src/main/java/bank/filter/AuthTokenFilter.java
@@ -0,0 +1,70 @@
+package bank.filter;
+
+import bank.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if (!hasAuthorizationBearer(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = getAccessToken(request);
+        if (!jwtUtil.validateJwtToken(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        setAuthenticationContext(token, request);
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean hasAuthorizationBearer(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        return authHeader != null && authHeader.startsWith("Bearer");
+    }
+
+    private String getAccessToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        return authHeader.substring(7);
+    }
+
+    private void setAuthenticationContext(String token, HttpServletRequest request) {
+        UserDetails userDetails = getUserDetails(token);
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.getAuthorities()
+        );
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private UserDetails getUserDetails(String token) {
+        return userDetailsService.loadUserByUsername(jwtUtil.getSubject(token));
+    }
+
+}
