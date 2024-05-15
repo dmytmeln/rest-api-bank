97faf1338d0ebee4af0c783da36bd5d6a81537db
Dmytro Melnyk <dimamel28@gmail.com>
2024-05-15 23:08:55 +0300

    Changed @PreAuthorize logic

--- src/main/java/bank/controllers/BankController.java
+++ src/main/java/bank/controllers/BankController.java
@@ -35,13 +35,13 @@ public class BankController {
     }
 
     @GetMapping("/{bankAccountId}")
-    @PreAuthorize("hasRole('ADMIN') OR @userServiceImpl.hasBankAccount(#user, #bankAccountId)")
+    @PreAuthorize("hasRole('ADMIN') OR @bankServiceImpl.findById(#user.id, #bankAccountId)")
     public ResponseEntity<?> findById(@AuthenticationPrincipal User user, @PathVariable Long bankAccountId) {
         return ResponseEntity.ok().body(bankService.findBankResponseById(bankAccountId, user.getId()));
     }
 
     @PostMapping("/{bankAccountId}/deposit")
-    @PreAuthorize("hasRole('ADMIN') OR @userServiceImpl.hasBankAccount(#user, #bankAccountId)")
+    @PreAuthorize("hasRole('ADMIN') OR @bankServiceImpl.findById(#user.id, #bankAccountId)")
     public ResponseEntity<?> makeDeposit(
             @AuthenticationPrincipal User user,
             @PathVariable Long bankAccountId,
@@ -51,7 +51,7 @@ public class BankController {
     }
 
     @PostMapping("/{bankAccountId}/withdrawal")
-    @PreAuthorize("hasRole('ADMIN') OR @userServiceImpl.hasBankAccount(#user, #bankAccountId)")
+    @PreAuthorize("hasRole('ADMIN') OR @bankServiceImpl.findById(#user.id, #bankAccountId)")
     public ResponseEntity<?> makeWithdrawal(
             @AuthenticationPrincipal User user,
             @PathVariable Long bankAccountId,
@@ -61,7 +61,7 @@ public class BankController {
     }
 
     @DeleteMapping("/{bankAccountId}")
-    @PreAuthorize("hasRole('ADMIN') OR @userServiceImpl.hasBankAccount(#user, #bankAccountId)")
+    @PreAuthorize("hasRole('ADMIN') OR @bankServiceImpl.findById(#user.id, #bankAccountId)")
     public ResponseEntity<?> delete(@AuthenticationPrincipal User user, @PathVariable Long bankAccountId) {
         bankService.deleteBankAccount(bankAccountId, user.getId());
         return ResponseEntity.noContent().build();
